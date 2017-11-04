68ea4c6ee1e48beda0ff43085c8faeceb3318c54
ntiss <nicolas.tisserand@free.fr>
2017-11-04 16:17:36 +0100

    Activate rules in Sonar way profile

--- src/main/java/com/arkea/satd/sonar/ApigeePlugin.java
+++ src/main/java/com/arkea/satd/sonar/ApigeePlugin.java
@@ -1,11 +1,31 @@
+/*
+ * SonarQube Apigee Plugin
+ * @author Nicolas Tisserand
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package com.arkea.satd.sonar;
 
 import org.sonar.api.Plugin;
 
 import com.arkea.satd.sonar.python.ApigeePythonRulesDefinition;
 import com.arkea.satd.sonar.python.ApigeePythonSensor;
+import com.arkea.satd.sonar.python.PythonSonarWayProfile;
 import com.arkea.satd.sonar.xml.ApigeeXmlRulesDefinition;
 import com.arkea.satd.sonar.xml.ApigeeXmlSensor;
+import com.arkea.satd.sonar.xml.XmlSonarWayProfile;
 
 public class ApigeePlugin implements Plugin {
 
@@ -15,33 +35,16 @@ public class ApigeePlugin implements Plugin {
 	public void define(Context context) {
 
 		context.addExtensions(
-			//Xml.class,
+			//Xml.class, // Already imported by sonar-xml-plugin
 			ApigeeXmlRulesDefinition.class,
 			ApigeeXmlSensor.class,
+			XmlSonarWayProfile.class,
+
+			//Python.class, // Already imported by sonar-python-plugin
 			ApigeePythonSensor.class,
-			ApigeePythonRulesDefinition.class
+			ApigeePythonRulesDefinition.class,
+			PythonSonarWayProfile.class
 		);
 	}
 
 }
-
-
-/*
-	LOG.info("ApigeePlugin ########################################");
-	List<Class<?>> newExtensions = new ArrayList<Class<?>>();
-	newExtensions.add(ApigeeRulesDefinition.class);
-	newExtensions.add(ApigeeSensor.class);
-	
-	List<Class<?>> currentExtensions = context.getExtensions();
-	
-	for(Class c : currentExtensions) {
-		LOG.info("extension : " + c + " - " + c.getName());
-	}
-	
-	
-	if(!currentExtensions.contains(Xml.class)) {
-		newExtensions.add(Xml.class);
-	}
-	
-	context.addExtensions(newExtensions);
-*/
---
+++ src/main/java/com/arkea/satd/sonar/xml/XmlSonarWayProfile.java
@@ -0,0 +1,64 @@
+/*
+ * SonarQube Apigee XML Plugin
+ * @author Nicolas Tisserand
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.arkea.satd.sonar.xml;
+
+import org.sonar.api.profiles.ProfileDefinition;
+import org.sonar.api.profiles.RulesProfile;
+import org.sonar.api.rules.Rule;
+import org.sonar.api.rules.RuleFinder;
+import org.sonar.api.utils.AnnotationUtils;
+import org.sonar.api.utils.ValidationMessages;
+import org.sonar.plugins.xml.language.Xml;
+
+import com.arkea.satd.sonar.ApigeePlugin;
+import com.arkea.satd.sonar.xml.checks.CheckRepository;
+
+/**
+ * Default XML profile.
+ * 
+ * @author Matthijs Galesloot
+ */
+public final class XmlSonarWayProfile extends ProfileDefinition {
+
+  private final RuleFinder ruleFinder;
+
+  public XmlSonarWayProfile(RuleFinder ruleFinder) {
+    this.ruleFinder = ruleFinder;
+  }
+
+  @Override
+  public RulesProfile createProfile(ValidationMessages validation) {
+    RulesProfile profile = RulesProfile.create(ApigeePlugin.SONAR_WAY_PROFILE_NAME, Xml.KEY);
+    
+    for(Object ruleClass : CheckRepository.getChecks()) {
+    	org.sonar.check.Rule ruleAnnotation = AnnotationUtils.getAnnotation(ruleClass, org.sonar.check.Rule.class);
+	    if (ruleAnnotation != null) {
+	      String ruleKey = ruleAnnotation.key();
+	      Rule rule = ruleFinder.findByKey(CheckRepository.REPOSITORY_KEY, ruleKey);
+	      
+	      profile.activateRule(rule, null);
+	      
+	    }
+    }
+    return profile;
+  }
+
+
+
+}
