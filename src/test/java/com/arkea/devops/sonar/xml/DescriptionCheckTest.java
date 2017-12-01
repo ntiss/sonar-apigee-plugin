0a7d666e4d96172d9287de2e7c52d26e5368d722
ntiss <nicolas.tisserand@free.fr>
2017-12-01 10:47:08 +0100

    Licensing under Apache v2.0

--- src/test/java/com/arkea/devops/sonar/xml/DescriptionCheckTest.java
+++ src/test/java/com/arkea/devops/sonar/xml/DescriptionCheckTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Credit Mutuel Arkea
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
 package com.arkea.devops.sonar.xml;
 
 import static org.junit.Assert.assertEquals;
@@ -11,6 +26,11 @@ import org.sonar.plugins.xml.checks.XmlIssue;
 import org.sonar.plugins.xml.checks.XmlSourceCode;
 
 public class DescriptionCheckTest extends AbstractCheckTester {
+
+	private List<XmlIssue> getIssues(String content) throws IOException {
+		XmlSourceCode sourceCode = parseAndCheck(createTempFile(content), new DescriptionCheck());
+		return sourceCode.getXmlIssues();
+	}
 
 	@Test
 	public void test_ok() throws Exception {
@@ -31,10 +51,5 @@ public class DescriptionCheckTest extends AbstractCheckTester {
 		);
 		assertEquals(1, issues.size());
 	}	
-	
-	private List<XmlIssue> getIssues(String content) throws IOException {
-		XmlSourceCode sourceCode = parseAndCheck(createTempFile(content), new DescriptionCheck());
-		return sourceCode.getXmlIssues();
-	}
 
 }
